008c80ef661fc1d69d455f615ced89aac592f094
Manu <dominguez.dieguez.manuel.jesus@iescamas.es>
2025-12-16 18:44:41 +0100

    Hola mundo

---
+++ EntradaSalida/HolaMundo/main.cpp
@@ -0,0 +1,15 @@
+//para que el sistema incluya la libreria estandar de entrada y salida.
+#include <iostream>
+
+/**
+ * metodo principal. devuelve un entero que puede ser 0 si no hay fallos
+ * o cualquier otro numero en el caso de que los haya
+*/
+int main() {
+    /**
+     * cout: salida de caracteres.
+     * endl: final de linea. inserta un salto de linea y limpia el buffer
+     */
+    std::cout <<"Hola mundo" <<std::endl;
+    return 0;
+}
