5f030ed3556015490e74467362205cab7379559f
Manu <dominguez.dieguez.manuel.jesus@iescamas.es>
2025-12-18 12:31:46 +0100

    Ejercicio vectores

--- EjerciciosBasicos/analisistemperaturas.cpp
+++ EjerciciosBasicos/analisistemperaturas.cpp
@@ -8,27 +8,37 @@ AnalisisTemperaturas::AnalisisTemperaturas() {}
 
     void AnalisisTemperaturas::introducir() {
         double election;
-        do {
+        while (true) {
             std::cout << "Introduzca una temperatura (0 para salir): " << std::endl;
             std::cin >> election;
+            if (election == 0) {
+                break;
+            }
             temperaturas.push_back(election);
-        } while (election != 0);
+        }
 
         std::cout << "Las temperaturas introducidas son: " << std::endl;
-
         for (int i = 0; i < temperaturas.size(); i++) {
             std::cout << "Temperatura "  <<i<< ":" << temperaturas[i] <<std::endl;
         }
     }
 
     void AnalisisTemperaturas::calcularMaxima() {
-        std::sort(temperaturas.end(), temperaturas.begin());
-        double tempMax = temperaturas[0];
-        std::cout << "La temperatura máxima es de: " << tempMax << std::endl;
+        if (temperaturas.empty()) {
+            std::cout << "No existen registros" << std::endl;
+        } else {
+            std::sort(temperaturas.begin(), temperaturas.end());
+            double tempMax = temperaturas[temperaturas.size() - 1];
+            std::cout << "La temperatura máxima es de: " << tempMax << std::endl;
+        }
     }
 
     void AnalisisTemperaturas::calcularMinima() {
-        std::sort(temperaturas.begin(), temperaturas.end());
-        double tempMin = temperaturas[0];
-        std::cout << "La temperatura mínima es de: " << tempMin << std::endl;
+        if (temperaturas.empty()) {
+            std::cout << "No hay registros" << std::endl;
+        } else {
+            std::sort(temperaturas.begin(), temperaturas.end());
+            double tempMin = temperaturas[0];
+            std::cout << "La temperatura mínima es de: " << tempMin << std::endl;
+        }
     }
